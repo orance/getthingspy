a0bf20e5d78f40fb9c0d51d74de8388e981405db
chengweiguang <405909462@qq.com>
2017-09-08 20:16:30 +0800

    add beautifulsoup_demo files

--- cookieread_demo.py
+++ cookieread_demo.py
@@ -1,7 +1,7 @@
 import cookielib
 import urllib2
 cookie = cookielib.MozillaCookieJar()
-cookie,load('cookie.txt',ignore_discard = True,ignore_expires = True)
+cookie.load('cookie.txt',ignore_discard = True,ignore_expires = True)
 req = urllib2.Request('http://www.baidu.com')
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
 response = opener.open(req)
